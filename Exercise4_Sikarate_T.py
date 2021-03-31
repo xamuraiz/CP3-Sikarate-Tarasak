c5972be7972284f5b3ba0cfe07723584d4e71a31
xamuraiz <bsikarate@gmail.com>
2021-03-31 20:46:22 +0700

    Create Exercise
    Create first exercise for Python Course

---
+++ Exercise4_Sikarate_T.py
@@ -0,0 +1,14 @@
+print("Student_Score_For_2ndYear_Student")
+
+FD_Eng = 95.5
+Gen_Bus = 97.5
+Intro_CS = 90.5
+ComPro = 91.3
+
+print("-------------------------")
+print("Your Score")
+print("Foundation English : ", FD_Eng)
+print("Genral Business : ", Gen_Bus)
+print("Introduction to Computer System : ", Intro_CS)
+print("Computer Programing : ", ComPro)
+
